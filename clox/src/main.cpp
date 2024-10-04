f1830a6d8782e3a85e19b5f1ba6f764e98a2fdcc
Marcos Balsa <markisbcn@gmail.com>
2024-10-05 01:28:11 +0200

    add command support for compile/run/interpret

--- clox/src/main.cpp
+++ clox/src/main.cpp
@@ -1,4 +1,6 @@
-#include <cstdio>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 
 #include "common.h"
 #include "vm.h"
@@ -20,15 +22,53 @@ int main(int argc, const char* argv[])
     printf("<<<<<< Unit tests\n");
 #endif  // #if 0
 
-    VirtualMachine VM;
-    VM.init();
-    ScopedCallback vmFinish([&VM] { VM.finish(); });
-
+    Compiler compiler;
     Compiler::Configuration compilerConfiguration;
 
-#if USING(DEBUG_BUILD)
-    if (0)
+#if USING(PE_BUILD)
+    if (1)
     {
+        const char codeStr[] =
+            "CODE_SECTION_BEGIN>\
+-------------------------------------------------------------------------------------------------\
+-------------------------------------------------------------------------------------------------\
+-------------------------------------------------------------------------------------------------\
+-------------------------------------------------------------------------------------------------\
+-------------------------------------------------------------------------------------------------\
+-------------------------------------------------------------------------------------------------\
+-------------------------------------------------------------------------------------------------\
+-------------------------------------------------------------------------------------------------\
+-------------------------------------------------------------------------------------------------\
+-------------------------------------------------------------------------------------------------\
+-------------------------------------------------------------------------------------------------\
+-------------------------------------------------------------------------------------------------\
+-------------------------------------------------------------------------------------------------\
+-------------------------------------------------------------------------------------------------\
+-------------------------------------------------------------------------------------------------\
+-------------------------------------------------------------------------------------------------\
+-------------------------------------------------------------------------------------------------\
+-------------------------------------------------------------------------------------------------\
+-------------------------------------------------------------------------------------------------\
+-------------------------------------------------------------------------------------------------\
+-------------------------------------------------------------------------------------------------\
+-------------------------------------------------------------------------------------------------\
+-------------------------------------------------------------------------------------------------\
+<CODE_SECTION_END";
+        auto result = VM.interpret(codeStr, "APP_NAME", compilerConfiguration);
+        if (!result.isOk())
+        {
+            LOG_ERROR("%s\n", result.error().message().c_str());
+        }
+    }
+    else
+#endif  // #if USING(PE_BUILD)
+#if USING(DEBUG_BUILD)
+        if (0)
+    {
+        VirtualMachine VM;
+        VM.init();
+        ScopedCallback vmFinish([&VM] { VM.finish(); });
+
         // const char codeStr[] = "(-1 + 2) - 4 * 3 / ( -5 - 6 + 35)";
         // const char codeStr[] = "print !true";
         // const char codeStr[] = "!(5 - 4 > 0 * 2 == !false)";
@@ -57,11 +97,14 @@ int main(int argc, const char* argv[])
             const char* desc;
             enum class Type
             {
+                help,
                 code,
                 default_const_variables,
                 allow_dynamic_variables,
                 repl,
-                help,
+                compile,
+                run,
+                output,
             };
             Type type;
             const char* params = nullptr;
@@ -69,18 +112,21 @@ int main(int argc, const char* argv[])
 #define ADD_PARAM(TYPE, DESC) {#TYPE, DESC, Param::Type::TYPE}
 #define ADD_PARAM_WITH_PARAMS(TYPE, DESC, PARAMS) {#TYPE, DESC, Param::Type::TYPE, PARAMS}
         const Param params[] = {
-            ADD_PARAM_WITH_PARAMS(code, "Allows passing <source_code> as a character string", "<source_code>"),
+            ADD_PARAM(help, "Shows this help"),
             ADD_PARAM(allow_dynamic_variables,
                       "Allows dynamic creation of variables on use (i.e., variable declaration not required)"),
             ADD_PARAM(default_const_variables,
                       "Variables are const by default, requiring <mut> modifier to be writable"),
             ADD_PARAM(repl, "Enters interactive mode(i.e. REPL)"),
-            ADD_PARAM(help, "Shows this help"),
+            ADD_PARAM(compile, "Compiles into bytecode and outputs the result to console or the output_file defined"),
+            ADD_PARAM_WITH_PARAMS(output, "Allows defining the output file for -compile", "<output_file>"),
+            ADD_PARAM(run, "Runs the code in the <bytecode_file> through the VM"),
+            ADD_PARAM_WITH_PARAMS(code, "Allows passing <source_code> as a character string", "<source_code>"),
         };
 #undef ADD_PARAM
         auto showHelpFunc = [&]()
         {
-            fprintf(stderr, "Usage: %s [source_path]\n", argv[0]);
+            fprintf(stderr, "Usage: %s [arguments...] [source_path.clox]\n", argv[0]);
             size_t longerArg = 0;
             for (const Param& param : params)
             {
@@ -97,15 +143,32 @@ int main(int argc, const char* argv[])
 
         /////////////////////////////////////////////////////////////////////////////////
 
-        bool hasToShowHelp = false;
-        const char* srcCodeOrFile = nullptr;
-        bool isCodeOrFile = false;
-        bool isREPL = false;
+        enum class ExecutionMode
+        {
+            Compile,
+            Run,
+            Interpret,
+            REPL,
+        };
+        enum class OutputMode
+        {
+            File,
+            Console
+        };
+        struct
+        {
+            bool hasToShowHelp = false;
+            const char* srcCodeOrFile = nullptr;
+            bool isCodeOrFile = false;
+            ExecutionMode mode = ExecutionMode::Interpret;
+            const char* compileOutputPath = nullptr;
+        } config;
 
+        auto isArgFunc = [](const char* arg) { return (arg[0] == '-'); };
         for (const char** argvPtr = &argv[1]; argvPtr != &argv[argc]; ++argvPtr)
         {
             const char* argv = *argvPtr;
-            if (argv[0] == '-')
+            if (isArgFunc(argv))
             {
                 bool validParam = false;
                 for (const Param& param : params)
@@ -117,8 +180,28 @@ int main(int argc, const char* argv[])
                         switch (param.type)
                         {
                             case Param::Type::code:
-                                isCodeOrFile = true;
+                            {
+                                config.isCodeOrFile = true;
+                                if (!isArgFunc(*(argvPtr + 1)))
+                                {
+                                    config.srcCodeOrFile = *(++argvPtr);
+                                }
                                 break;
+                            }
+                            case Param::Type::output:
+                            {
+                                if (config.mode != ExecutionMode::Compile)
+                                {
+                                    LOG_ERROR("Unexpected output file. Only used with -compile: %s", argv);
+                                    validParam = false;
+                                    break;
+                                }
+                                if (!isArgFunc(*(argvPtr + 1)))
+                                {
+                                    config.compileOutputPath = *(++argvPtr);
+                                }
+                                break;
+                            }
                             case Param::Type::allow_dynamic_variables:
                                 compilerConfiguration.allowDynamicVariables = true;
                                 break;
@@ -126,10 +209,16 @@ int main(int argc, const char* argv[])
                                 compilerConfiguration.defaultConstVariables = true;
                                 break;
                             case Param::Type::repl:
-                                isREPL = true;
+                                config.mode = ExecutionMode::REPL;
                                 break;
                             case Param::Type::help:
-                                hasToShowHelp = true;
+                                config.hasToShowHelp = true;
+                                break;
+                            case Param::Type::compile:
+                                config.mode = ExecutionMode::Compile;
+                                break;
+                            case Param::Type::run:
+                                config.mode = ExecutionMode::Run;
                                 break;
                             default:
                                 LOG_ERROR("Invalid parameter: %s", argv);
@@ -141,31 +230,35 @@ int main(int argc, const char* argv[])
                 if (!validParam)
                 {
                     LOG_ERROR("Invalid parameter: %s\n", argv);
-                    hasToShowHelp = true;
+                    config.hasToShowHelp = true;
                     resultCode = -1;
                     break;
                 }
             }
             else
             {
-                if (srcCodeOrFile != nullptr)
+                if (config.srcCodeOrFile != nullptr)
                 {
                     LOG_ERROR("Parameter '%s' is unexpected, <%s> is already defined: '%s'\n", argv,
-                              isCodeOrFile ? "source_code" : "source_path", srcCodeOrFile);
-                    hasToShowHelp = true;
+                              config.isCodeOrFile ? "source_code" : "source_path", config.srcCodeOrFile);
+                    config.hasToShowHelp = true;
                     resultCode = -1;
                     break;
                 }
-                srcCodeOrFile = *argvPtr;
+                config.srcCodeOrFile = *argvPtr;
             }
         }
 
-        if (hasToShowHelp)
+        if (config.hasToShowHelp)
         {
             showHelpFunc();
         }
-        else if (isREPL)
+        else if (config.mode == ExecutionMode::REPL)
         {
+            VirtualMachine VM;
+            VM.init();
+            ScopedCallback vmFinish([&VM] { VM.finish(); });
+
 #if USING(DEBUG_BUILD)
             util::SetDebugBreakEnabled(false);
 #endif  // #if USING(DEBUG_BUILD)
@@ -178,32 +271,116 @@ int main(int argc, const char* argv[])
         }
         else
         {
-            if (!srcCodeOrFile)
+            if (config.srcCodeOrFile == nullptr)
             {
                 resultCode = -1;
-                LOG_ERROR("Missing %s\n", isCodeOrFile ? "source_code parameter" : "source_path");
+                LOG_ERROR("Missing %s\n", config.isCodeOrFile ? "source_code parameter" : "source_path");
                 showHelpFunc();
-            }
-            else if (isCodeOrFile)
-            {
-                auto result = VM.runFromSource(srcCodeOrFile, compilerConfiguration);
-                if (!result.isOk())
-                {
-                    resultCode = -1;
-                    LOG_ERROR(result.error().message().c_str());
-                }
             }
             else
             {
-                auto result = VM.runFromFile(srcCodeOrFile, compilerConfiguration);
-                if (!result.isOk())
+                if (config.mode == ExecutionMode::Compile)
                 {
-                    resultCode = -1;
-                    LOG_ERROR(result.error().message().c_str());
+                    auto checkResult = [&resultCode](const Compiler::result_t& result) -> bool
+                    {
+                        if (!result.isOk())
+                        {
+                            resultCode = -1;
+                            LOG_ERROR(result.error().message().c_str());
+                            return false;
+                        }
+                        return true;
+                    };
+                    std::unique_ptr<Chunk> compiledChunk = nullptr;
+                    if (config.isCodeOrFile)
+                    {
+                        auto result = compiler.compileFromSource(config.srcCodeOrFile, compilerConfiguration);
+                        if (checkResult(result))
+                        {
+                            compiledChunk = result.extract();
+                        }
+                    }
+                    else
+                    {
+                        auto result = compiler.compileFromFile(config.srcCodeOrFile, compilerConfiguration);
+                        if (checkResult(result))
+                        {
+                            compiledChunk = result.extract();
+                        };
+                    }
+
+                    if (compiledChunk != nullptr)
+                    {
+                        if (config.compileOutputPath != nullptr)
+                        {
+                            std::ofstream ofs(config.compileOutputPath,
+                                              std::ofstream::binary | std::ofstream::trunc | std::ofstream::out);
+                            if (ofs.bad())
+                            {
+                                LOG_ERROR("Failed to open file '%s' for writing\n", config.compileOutputPath);
+                                return -1;
+                            }
+                            else
+                            {
+                                compiledChunk->serialize(ofs);
+                            }
+                        }
+                        else
+                        {
+                            compiledChunk->serialize(std::cout);
+                        }
+                    }
+                }
+                else if (config.mode == ExecutionMode::Run)
+                {
+                    VirtualMachine VM;
+                    VM.init();
+                    ScopedCallback vmFinish([&VM] { VM.finish(); });
+
+                    Chunk code(config.isCodeOrFile ? "CODE" : config.srcCodeOrFile);
+                    if (config.isCodeOrFile)
+                    {
+                        std::istringstream is(config.srcCodeOrFile);
+                        code.deserialize(is);
+                    }
+                    else
+                    {
+                        std::ifstream ifs(config.srcCodeOrFile, std::ifstream::binary);
+                        if (ifs.good())
+                        {
+                            code.deserialize(ifs);
+                            disassemble(code, config.srcCodeOrFile);
+                        }
+                        else
+                        {
+                            LOG_ERROR("Failed to open file '%s' for reading\n", config.compileOutputPath);
+                            resultCode = -1;
+                        }
+                    }
+
+                    auto result = VM.runFromByteCode(code);
+                    if (!result.isOk())
+                    {
+                        resultCode = -1;
+                        LOG_ERROR(result.error().message().c_str());
+                    }
+                }
+                else if (config.mode == ExecutionMode::Interpret)
+                {
+                    VirtualMachine VM;
+                    VM.init();
+                    ScopedCallback vmFinish([&VM] { VM.finish(); });
+
+                    auto result = config.isCodeOrFile ? VM.runFromSource(config.srcCodeOrFile, compilerConfiguration)
+                                                      : VM.runFromFile(config.srcCodeOrFile, compilerConfiguration);
+                    if (!result.isOk())
+                    {
+                        resultCode = -1;
+                        LOG_ERROR(result.error().message().c_str());
+                    }
                 }
             }
         }
     }
-
     return resultCode;
 }
