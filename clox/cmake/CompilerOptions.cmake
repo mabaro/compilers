2f5f6c7e8f0b640a71d67cb7f339747bc9e063d6
Marcos Balsa <markisbcn@gmail.com>
2024-10-06 22:14:46 +0200

    retry fix

--- clox/cmake/CompilerOptions.cmake
+++ clox/cmake/CompilerOptions.cmake
@@ -69,8 +69,8 @@ else()
     endif()
 
     # disable warnings
-    ADD_IF_NOT_PRESSENT(CMAKE_CXX_FLAGS_INIT "-Wno-error=gnu-zero-variadic-macro-arguments")
-    ADD_IF_NOT_PRESSENT(CMAKE_CXX_FLAGS_INIT "-Wno-error=format-security")
+    ADD_IF_NOT_PRESSENT(CMAKE_CXX_FLAGS_INIT "-Wno-gnu-zero-variadic-macro-arguments")
+    ADD_IF_NOT_PRESSENT(CMAKE_CXX_FLAGS_INIT "-Wno-format-security")
     ADD_IF_NOT_PRESSENT(CMAKE_CXX_FLAGS_INIT "-Wno-error=unused-function")
 
     string(REPLACE ";" " " CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT}")
