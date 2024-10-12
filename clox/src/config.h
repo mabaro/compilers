dcc99a616663c120c78997d70f58d47f5c09426f
marcos <markisbcn@gmail.com>
2024-10-12 19:14:35 +0200

    add PE build target

--- clox/src/config.h
+++ clox/src/config.h
@@ -15,7 +15,7 @@
 #ifndef DEBUG
 #if !USING(RELEASE_BUILD)
 #error Either NDEBUG or DEBUG must be defined!
-#endif // #if !USING(RELEASE_BUILD)
+#endif  // #if !USING(RELEASE_BUILD)
 #define DEBUG_BUILD NOT_IN_USE
 #else
 #if USING(RELEASE_BUILD)
@@ -24,10 +24,28 @@
 #define DEBUG_BUILD IN_USE
 #endif  // #if DEBUG
 
-// PE build is intended for injecting bytecodes into a template VM binary
+////////////////////////////////////////////////////////////////////////////////
+// PE build is intended for injecting bytecode into a template VM binary
+#ifdef PE_BUILD
+////////////////////////////////////////////////////////////////////////////////
+#undef PE_BUILD
+#define PE_BUILD IN_USE
+// #pragma optimize("gsy",on)
+// #pragma comment(linker,"/merge:.rdata=.data")
+// #pragma comment(linker,"/merge:.text=.data")
+// #pragma comment(linker,"/merge:.reloc=.data")
+#ifdef WINDOWS
+#pragma comment(linker, "/OPT:NOWIN98")
+#endif  // #ifdef WINDOWS
+////////////////////////////////////////////////////////////////////////////////
+#else   // #if USING(PE_BUILD )
+////////////////////////////////////////////////////////////////////////////////
+#undef PE_BUILD
 #define PE_BUILD NOT_IN_USE
+#endif  // #else//#endif //#ifdef PE_BUILD
 static_assert(!USING(PE_BUILD) || !USING(DEBUG_BUILD));
-
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 #define MACHINE_BITS 8
 #define MAX_OPCODE_BITS MACHINE_BITS
