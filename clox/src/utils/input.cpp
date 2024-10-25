bd520a89df0fdc551a3d974dfeff7a5b411a782c
marcos <markisbcn@gmail.com>
2024-10-26 01:55:29 +0200

    add read_Char

---
+++ clox/src/utils/input.cpp
@@ -0,0 +1,21 @@
+#include "input.h"
+#include "config.h"
+
+#if defined(LINUX_OS)
+#include <cstdlib>
+#include <cstdio>
+#elif defined(WINDOWS_OS)
+#include <conio.h> //getch()
+#endif
+
+char read_char()
+{
+#if defined(LINUX_OS)
+    system("stty raw");
+    const char c = getchar();
+    system("stty cooked");
+#elif defined(WINDOWS_OS)
+    const char c = getch();
+#endif
+    return c;
+}
---
+++ clox/src/utils/input.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// Platform independent read-char from keyboard without waiting for enter
+char read_char();
